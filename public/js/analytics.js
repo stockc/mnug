94054ca9d835889c1c96c7d1277572d1e6830277
Andreas Greimel <andreas.greimel@tngtech.com>
2015-10-27 18:34:51 +0100

    Honor do not track

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -11,4 +11,5 @@
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
 ga('create', 'UA-69336451-3', 'auto');
-ga('send', 'pageview');
+ga('require', 'dnt')
+;ga('send', 'pageview');
